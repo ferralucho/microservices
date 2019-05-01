8194a86e8c64e590311e40e862137fb52418fdb6
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-05-01 20:36:07 -0300

    Using feign rest client for service invocation

--- currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.ferralucho.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.ferralucho.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {

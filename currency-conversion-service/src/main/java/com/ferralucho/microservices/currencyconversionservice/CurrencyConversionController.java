8f9cb5ed354461fe0dd0c72feea7ffdbeca74471
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-05-05 17:38:22 -0300

    Set up for request through zuul api gateway

--- currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.ferralucho.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -38,13 +41,14 @@ public class CurrencyConversionController {
 
     @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
-                                                  @PathVariable BigDecimal quantity) {
+                                                       @PathVariable BigDecimal quantity) {
 
-        CurrencyConversionBean response = proxy.retrieveExchangeValue(from,to);
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        logger.info("{}", response);
 
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
                 quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
-
 
 }
--- currency-exchange-service/src/main/java/com/ferralucho/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/ferralucho/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.ferralucho.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +19,18 @@ public class CurrencyExchangeController {
     @Autowired
     private ExchangeValueRepository repository;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
-        /*
+        ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+
         String portVal = environment.getProperty("server.port");
         if(portVal != null){
             exchangeValue.setPort(Integer.parseInt(portVal.trim()));
-        }*/
+        }
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }

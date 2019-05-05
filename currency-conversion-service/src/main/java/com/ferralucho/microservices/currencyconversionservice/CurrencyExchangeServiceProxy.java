bcc15d5e5f258e5e2be224fadaab1c6987682fc7
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-05-05 15:53:20 -0300

    Distributing calls using Eureka and Ribbon

--- currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/ferralucho/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url = "localhost:8000")
-@FeignClient(name="currency-exchange-service", url = "localhost:8000")
+@FeignClient(name="currency-exchange-service")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
